95cf74629cf228705451c89087d8e5c29ef2a4cc
Dominik Kotecki <dominikus1910@gmail.com>
2018-02-19 20:06:14 +0100

    config

--- src/MigrationsPostgreSql/Program.fs
+++ src/MigrationsPostgreSql/Program.fs
@@ -1,27 +1,19 @@
 namespace MigrationsPostgreSql
 open System.Reflection
-open Argu
 open Npgsql
 open SimpleMigrations.DatabaseProvider
 open SimpleMigrations
 open SimpleMigrations.Console
-
-type ServiceConfig =
-    | PostgresConnectionString of conn: string
-with 
-    interface IArgParserTemplate with
-        member x.Usage =
-            match x with
-            | PostgresConnectionString _ -> "ConnectionString do bazy danych"
+open Microsoft.Extensions.Configuration
 
 module Program =
 
     [<EntryPoint>]
     let main argv =
-        let parser = ArgumentParser.Create<ServiceConfig>(programName = "MigrationsPostgreSql.exe")
-        let result = parser.Parse()
+        let builder = ConfigurationBuilder().AddJsonFile("appsettings.json", optional = false, reloadOnChange = true);
+        let config = builder.Build()
         let assembly = Assembly.GetExecutingAssembly()
-        use db = new NpgsqlConnection(result.GetResult PostgresConnectionString)
+        use db = new NpgsqlConnection(config.GetValue<string>("Data:Postgres:ConnectionString"))
         let provider = PostgresqlDatabaseProvider(db)
         let migrator = SimpleMigrator(assembly, provider)
         let consoleRunner = ConsoleRunner(migrator)
